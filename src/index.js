2157eda98b59ae45b975fdbea9f70ef052eaad97
wukapHo <xb0ksx@gmail.com>
2021-03-13 21:02:18 +0300

    docs: edit index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === "undefined" || array.length === 0) {
+    return 0;
+  }
+
+  let minValue = array.reduce((prevValue, item) => {
+    if (prevValue > item) {
+      prevValue = item;
+    }
+    return prevValue;
+  }, array[0])
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === "undefined" || array.length === 0) return 0;
+  return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  if(typeof array === "undefined" || array.length === 0) return 0;
+
+  let sum = array.reduce((sum, item) => {
+    return sum + item;
+  }, 0)
+
+  return sum / array.length;
 }
